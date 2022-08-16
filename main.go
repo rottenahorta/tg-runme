b4cdbd30c727edea8e3e109668a83dd89d9b1113
rottenahorta <dah41kj@gmail.com>
2022-08-16 17:36:34 +1000

    removed godotenv

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 	tg "github.com/rottenahorta/tgbotsche/pkg/telegram"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	//err := godotenv.Load()
+	//if err != nil {
+	//	log.Fatal("Error loading .env file")
+	//}
 	bot := tg.NewBot("api.telegram.org", os.Getenv("TOKEN"), 100) //, "https://tg-runme.herokuapp.com/")
 	log.Printf("bot start")
 	bot.SetWH("https://tg-runme.herokuapp.com/webhook/" + os.Getenv("TOKEN"))
