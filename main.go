949a26ec9f846b50f752dad1b927a0dd8c5b6605
rottenahorta <dah41kj@gmail.com>
2022-08-17 11:34:39 +1000

    fetchin updates fix

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	//bot := tg.NewBot("api.telegram.org", os.Getenv("TOKEN"), 100) //, "https://tg-runme.herokuapp.com/") // add WH host as new var, delete l var
 	bot := tg.NewBot("tg-runme.herokuapp.com/webhook/", os.Getenv("TOKEN"), 100)
-	log.Printf("bot start")
+	log.Printf("bot start debug")
 	/*wh := "https://tg-runme.herokuapp.com/webhook/bot" + os.Getenv("TOKEN")
 	err := bot.SetWH(wh)
 	if err != nil {
