b4ee508b8cc3bc70647255dcea78d5d0e6f4be12
rottenahorta <dah41kj@gmail.com>
2022-08-17 14:47:51 +1000

    fetchin updates fix

--- pkg/telegram/client.go
+++ pkg/telegram/client.go
@@ -3,7 +3,7 @@ package tg
 import (
 	"encoding/json"
 	"io"
-	"net"
+	//"net"
 	"net/http"
 	"net/url"
 	"path"
@@ -63,11 +63,11 @@ func (c *Client) Update() ([]Update, error) {
 			return
 		}
 	}
-	l, err := net.Listen("tcp", c.listenPort)
+	/*l, err := net.Listen("tcp", c.listenPort)
 	if err != nil {
 		return nil, err
-	}
-	go http.Serve(l, http.HandlerFunc(handler))
+	}*/
+	go http.ListenAndServe(c.path, http.HandlerFunc(handler))
 	return res.Result, nil
 
 	/*var res UpdateResponse
