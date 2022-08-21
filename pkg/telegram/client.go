423d42946fd91f14dd4f6f6636bca00310c4102c
rottenahorta <dah41kj@gmail.com>
2022-08-21 19:47:11 +1000

    fixin zepp req

--- pkg/telegram/client.go
+++ pkg/telegram/client.go
@@ -70,7 +70,7 @@ func (c *Client) GetZeppData() (zp.Update, error) {
 	if err != nil {
 		return  zp.Update{}, er.Log("cant get zepp data", err)
 	}
-	log.Printf("zepp trq body: %v", b)
+	log.Printf("zepp req body: %v", string(b))
 	if err := json.Unmarshal(b, &res); err != nil {
 		return  zp.Update{}, er.Log("cant unmarshal zepp data", err)
 	}
