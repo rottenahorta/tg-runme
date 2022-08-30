2aa270adbd164a662f41f4e2ca51dae821eb0a6b
rottenahorta <dah41kj@gmail.com>
2022-08-30 17:57:49 +1000

    db zptoken v01_2

--- pkg/repo/schema.go
+++ pkg/repo/schema.go
@@ -3,5 +3,5 @@ package repo
 var initUsersDB = `CREATE TABLE IF NOT EXISTS users(
     id SERIAL NOT NULL UNIQUE,
     chatid int NOT NULL UNIQUE,
-    zptoken VARCHAR(255),
+    zptoken VARCHAR(255)
 )`
